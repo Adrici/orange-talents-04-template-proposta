7614c0482a60af7f4afb61748bd24a15b5b7a3d5
Adrici <cirellia5@gmail.com>
2021-05-13 14:52:38 -0300

    Consultas no Banco de dados

--- proposta/src/main/java/br/com/zup/proposta/novaproposta/PropostaRepository.java
+++ proposta/src/main/java/br/com/zup/proposta/novaproposta/PropostaRepository.java
@@ -9,7 +9,7 @@ public interface PropostaRepository extends JpaRepository<PropostaModel, Long> {
 	
 //Para Consultas no Banco de Dados : método findById - Já existe por Default
 //	Usando o Optional, porque ele pode ou nao existir, de acordo com a consulta no banco.
-//Usariamos Collection caso a fizemos a consulta com mais de um atributo no parâmero - 
+//Usariamos Collection caso fizemos a consulta com mais de um atributo no parâmetro - 
 	//Exemplo: findByNameAndDocumento (name, documento).
 
 } 
