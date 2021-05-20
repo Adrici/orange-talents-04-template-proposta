e81cdb3717e93859bbc8772183eabdf3ce789af1
Adrici <cirellia5@gmail.com>
2021-05-20 11:42:21 -0300

    Métricas-Prometheus

--- proposta/src/main/java/br/com/zup/proposta/bloqueio/BloqueioController.java
+++ proposta/src/main/java/br/com/zup/proposta/bloqueio/BloqueioController.java
@@ -4,7 +4,6 @@ import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
-
 import br.com.zup.proposta.cartao.CartaoClienteFeing;
 import br.com.zup.proposta.cartao.CartaoModel;
 import br.com.zup.proposta.cartao.CartaoRepository;
